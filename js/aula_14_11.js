35e02488961a84777a76aa0c181a251352e1467c
Leonardo <Leonardo.sousa.dev@gmail.com>
2022-11-17 13:38:08 -0300

    aula js pasta add img add

---
+++ js/aula_14_11.js
@@ -0,0 +1,10 @@
+//nome = prompt("qual é seu nome");
+
+//alert("Seja bem vindo a aula 14.11 " + nome);
+
+numero01 = prompt("Informe o primeiro numero!");
+numero02 = prompt("Informe o segundo numero!");
+
+soma = Number(numero01) + Number(numero02);
+
+alert("A soma dos dois numero é " + soma);
